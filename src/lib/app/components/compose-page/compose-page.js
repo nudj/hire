e5aabd44f3c2c7722ed53c8b8622817c753b96c1
Nick Collings <nick@collingo.com>
2017-06-02 12:39:36 +0100

    Update email rendering to match wysiwyg

--- src/lib/app/components/compose-page/compose-page.js
+++ src/lib/app/components/compose-page/compose-page.js
@@ -123,7 +123,7 @@ module.exports = class ComposePage extends React.Component {
     return <span className={ok ? this.style.tagOk : this.style.tagError} key={`chunk${index}`}>{contents}</span>
   }
   pify (para, index, margin = 0) {
-    return <p className={this.style.para} style={{ marginTop: cssVariables.sizing.textEditorLineHeight }} key={`para${index}`}>{para}</p>
+    return <p className={this.style.para} style={{ marginTop: `${1.5*margin}rem` }} key={`para${index}`}>{para}</p>
   }
   renderMessage (template) {
     return templater.render({
