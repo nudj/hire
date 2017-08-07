5a0754f2bf8820be4783642bfc38bd386a987be4
Wayne Durack <wayne.durack@gmail.com>
2017-08-07 10:57:34 +0100

    Squashed commit of the following:
    commit 5bab357bdc7bb8b6b2d1fe2a2a53f1fb4ea37db0
    Author: Wayne Durack <wayne.durack@gmail.com>
    Date:   Thu Jul 6 14:37:07 2017 +0100
    
        Peer review changes
    
    commit 2f4b671192cabc5b010106ae00b718f3c4ee5ec9
    Author: Wayne Durack <wayne.durack@gmail.com>
    Date:   Wed Jul 5 16:35:25 2017 +0100
    
        Unfuck tests
    
    commit 1611e7a19d3ddfe9f31082ac8a7fd5b9adf3cfd6
    Author: Wayne Durack <wayne.durack@gmail.com>
    Date:   Wed Jul 5 16:33:13 2017 +0100
    
        Fixed bug if no referrals
    
    commit 9e316e57cffef8f264f671369b3d39cf74a217be
    Author: Wayne Durack <wayne.durack@gmail.com>
    Date:   Wed Jul 5 16:32:57 2017 +0100
    
        Use table format
    
    commit 0fdb69ae0f152eb29177074edb5ad1f3e0318783
    Author: Wayne Durack <wayne.durack@gmail.com>
    Date:   Wed Jul 5 14:46:58 2017 +0100
    
        Aggregate counting of the full chain for applications and referrals
    
    commit 9a0ac26af450065a2b5e4ce1a871001b87deed2b
    Author: Wayne Durack <wayne.durack@gmail.com>
    Date:   Tue Jul 4 10:26:18 2017 +0100
    
        Referrals
    
    commit 45761b54e34f9424e4d3bd0ebec4a5451c88aabb
    Author: Wayne Durack <wayne.durack@gmail.com>
    Date:   Thu Jun 29 17:02:02 2017 +0100
    
        Aggregate job activity
    
    commit 426c832b22686302d00ab457070d81cecf961d7e
    Author: Wayne Durack <wayne.durack@gmail.com>
    Date:   Thu Jun 29 10:36:29 2017 +0100
    
        Changes for resending messages
    
    commit 4e5c032429b3b822feff277b7245c0bdba5c6d06
    Author: Wayne Durack <wayne.durack@gmail.com>
    Date:   Wed Jun 28 16:11:39 2017 +0100
    
        Resending messages
    
    commit badbde9e1365aec9411f95048904a4c0489bcbd1
    Author: Wayne Durack <wayne.durack@gmail.com>
    Date:   Tue Jun 27 11:42:42 2017 +0100
    
        Add dates to mock data
    
    commit 07e056f38ca4e4bd7c9d4a0e4d35f781d6a87ea9
    Author: Wayne Durack <wayne.durack@gmail.com>
    Date:   Mon Jun 26 17:37:11 2017 +0100
    
        Show users that have been sent a message
    
    commit e894fc66f3532cdfbd509442272e8cc6d5a5123a
    Author: Wayne Durack <wayne.durack@gmail.com>
    Date:   Mon Jun 26 17:36:43 2017 +0100
    
        Move job page to nudj page

--- src/lib/app/components/job-page/job-page.js
+++ src/lib/app/components/job-page/job-page.js
@@ -1,16 +1,145 @@
 const React = require('react')
+const { Redirect } = require('react-router')
 const { Link } = require('react-router-dom')
 const get = require('lodash/get')
+const isEqual = require('lodash/isEqual')
 const { Helmet } = require('react-helmet')
 const getStyle = require('./job-page.css')
 const PageHeader = require('../page-header/page-header')
 const Tooltip = require('../tooltip/tooltip')
 const CopyToClipboard = require('../copy-to-clipboard/copy-to-clipboard')
 
+function renderJobActivitiy ({activity, style}) {
+  let trendStyle = style.jobActivityHighlightPositive
+
+  if (activity.trend === -1) {
+    trendStyle = style.jobActivityHighlightNegative
+  } else if (activity.trend === 0) {
+    trendStyle = style.jobActivityHighlightNeutral
+  }
+
+  return (<div className={style.jobActivity}>
+    <h4 className={style.jobActivityTitle}>{activity.title}</h4>
+    <p className={style.jobActivitySummary}><span className={trendStyle}>{activity.thisWeek}</span> in the last week</p>
+    <p className={style.jobActivityFooter}>{activity.total} in total</p>
+  </div>)
+}
+
+function getActivityData (title, data) {
+  const activityData = {
+    title: title,
+    thisWeek: data.thisWeek,
+    total: data.total,
+    trend: data.trend
+  }
+  return activityData
+}
+
+function getActivitesData ({props}) {
+  const pageViewData = get(props, 'activities.pageViews', {})
+  const referrerData = get(props, 'activities.referrers', {})
+  const applicationData = get(props, 'activities.applications', {})
+
+  const pageViews = getActivityData('Page views', pageViewData)
+  const referrers = getActivityData('Referrers', referrerData)
+  const applications = getActivityData('Applications', applicationData)
+
+  return {pageViews, referrers, applications}
+}
+
+function renderJobActivities ({props, style}) {
+  const {referrers, applications} = getActivitesData({props})
+
+  return (<div className={style.jobActivities}>
+    {renderJobActivitiy({ activity: referrers, style })}
+    {renderJobActivitiy({ activity: applications, style })}
+  </div>)
+}
+
+function renderSentListItem ({jobSlug, person, index, style}) {
+  const personId = get(person, 'id', '')
+  const firstName = get(person, 'firstName', '')
+  const lastName = get(person, 'lastName', '')
+  const source = get(person, 'source', '')
+  const applications = get(person, 'totalApplications', 0).toString()
+  const referrals = get(person, 'totalReferrals', 0).toString()
+
+  // internal - how to specify someone?
+  const resendLink = source === 'external' ? `/${jobSlug}/external/${get(person, 'id')}` : `/${jobSlug}/internal`
+  const resendButton = (<Link className={style.button} to={resendLink}>Resend</Link>)
+  const actions = source === 'referral' ? [] : [resendButton]
+
+  let status = '🤷‍'
+
+  if (applications !== '0') {
+    status = 'Referred applicant'
+  } else if (referrals !== '0') {
+    status = 'Added referrer'
+  }
+
+  // Need to change this RowItem to use a table or something - alignment and also column headings
+  return (<tr className={style.networkRow} key={`${personId}_${index}`}>
+    <td className={style.networkCellName}>{`${firstName} ${lastName}`}</td>
+    <td className={style.networkCellCapitalise}>{source}</td>
+    <td className={style.networkCell}>{status}</td>
+    <td className={style.networkCell}>{referrals}</td>
+    <td className={style.networkCell}>{applications}</td>
+    <td className={style.networkCellActions}>{actions}</td>
+  </tr>)
+}
+
+function renderSentList ({sent, props, style}) {
+  const jobSlug = get(props, 'job.slug', '')
+  return (<table className={style.network}>
+    <thead>
+      <tr className={style.networkRow}>
+        <th className={style.networkHead}>Name</th>
+        <th className={style.networkHead}>Source</th>
+        <th className={style.networkHead}>Status</th>
+        <th className={style.networkHead}>Referrals</th>
+        <th className={style.networkHead}>Applications</th>
+        <th className={style.networkHead} />
+      </tr>
+    </thead>
+    <tbody>
+      {sent.map((person, index) => renderSentListItem({jobSlug, person, index, style}))}
+    </tbody>
+  </table>)
+}
+
+function reduceSentComplete (accumulator, person) {
+  if (!Array.isArray(accumulator)) {
+    accumulator = [accumulator]
+  }
+
+  const exists = accumulator.filter(existing => isEqual(existing, person))
+
+  if (!exists.length) {
+    accumulator.push(person)
+  }
+
+  return accumulator
+}
+
 const JobPage = (props) => {
   const style = getStyle()
   const tooltip = get(props, 'tooltip')
+
+  const sentComplete = get(props, 'sentComplete', [])
+  const jobSlug = get(props, 'job.slug', '')
   const jobLink = `https://nudj.co/jobs/${get(props, 'company.slug')}+${get(props, 'job.slug')}`
+  const nudjLink = `/${jobSlug}/nudj`
+
+  if (!sentComplete.length) {
+    return (<Redirect to={nudjLink} push />)
+  }
+
+  // We only need one entry for each person
+  const sent = sentComplete.length > 1 ? sentComplete.reduce(reduceSentComplete) : sentComplete
+  const sentList = renderSentList({sent, props, style})
+  const plural = sent.length === 1 ? 'person' : 'people'
+
+  const jobActivity = renderJobActivities({props, style})
 
   return (
     <div className={style.pageBody}>
@@ -22,23 +151,18 @@ const JobPage = (props) => {
         subtitle={<span>@ <Link className={style.headerLink} to={'/'}>{get(props, 'company.name')}</Link></span>}
       >
         <CopyToClipboard className={style.copyLink} data-clipboard-text={jobLink}>Copy job link</CopyToClipboard>
+        <Link className={style.nudjLink} to={nudjLink}>nudj job</Link>
       </PageHeader>
-      <h3 className={style.pageHeadline}>Who would you like to ask first?</h3>
       <div className={style.pageContent}>
-        <div className={style.pageMain}>
-          <div className={style.internal}>
-            <div className={style.internalContent}>
-              <h4 className={style.title}>Ask your company</h4>
-              <p className={style.description}>Encourage your team to explore their networks, make recommendations and get your job out there.</p>
-            </div>
-            <Link className={style.button} to={`/${get(props, 'job.slug')}/internal`}>Select</Link>
+        <div className={style.pageMainContainer}>
+          <h3 className={style.pageHeadline}>Job activity</h3>
+          <div className={style.pageMain}>
+            {jobActivity}
           </div>
-          <div className={style.external}>
-            <div className={style.externalContent}>
-              <h4 className={style.title}>Ask your network</h4>
-              <p className={style.description}>We've identified key people from your network that will ensure your job gets in front of the best candidates.</p>
-            </div>
-            <Link className={style.button} to={`/${get(props, 'job.slug')}/external`}>Select</Link>
+          <hr className={style.sectionDivider} />
+          <h3 className={style.pageHeadline}>So far you've asked {sent.length} {plural}</h3>
+          <div className={style.pageMain}>
+            {sentList}
           </div>
         </div>
         <div className={style.pageSidebar}>
