b242606b5b75500b86661dd95c5fb81b66fda4a8
Timothy Robinson <timmyrobby@gmail.com>
2017-10-27 16:24:20 +0100

    Remove now-unused conversations module

--- src/app/components/layout-app/index.js
+++ src/app/components/layout-app/index.js
@@ -9,6 +9,8 @@ const ScrollTop = require('../scroll-top')
 const LayoutApp = (props) => {
   const style = getStyle()
 
+  console.log(props.history.action)
+
   return (
     <ScrollTop ignore={props.history.action === 'REPLACE'}>
       <div className={`${props.className} ${style.body}`}>
