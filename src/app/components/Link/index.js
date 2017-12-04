e8cab329c3e24e9fc123c1c7a2f88493f8754abf
Richard Palmer <rich@rdjpalmer.com>
2017-12-04 19:15:27 +0000

    Add react-router enabled link component

---
+++ src/app/components/Link/index.js
@@ -0,0 +1,21 @@
+// @flow
+const React = require('react')
+const { Link: RouterLink } = require('react-router-dom')
+const { Link: StyledLink } = require('@nudj/components')
+
+type Props = {
+  href: string,
+  restProps: Array<mixed>
+}
+
+const Link = ({ href, ...restProps }: Props) => (
+  <StyledLink
+    {...restProps}
+    href={href}
+    Component={({ href, ...restSubProps }: Props) => (
+      <RouterLink to={href} {...restSubProps} />
+    )}
+  />
+)
+
+module.exports = Link
