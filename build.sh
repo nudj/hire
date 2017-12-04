5b8039e1da2d9ad3a8555c51c6fd75679e3de4c0
Nick Collings <nick@collingo.com>
2017-12-04 18:52:20 +0000

    Fix yarn prod build

--- build.sh
+++ build.sh
@@ -5,5 +5,5 @@ docker build -t $1 --build-arg NPM_TOKEN=${NPM_TOKEN} -f ./Dockerfile.dev .
 docker run --rm --entrypoint cat $1 /usr/src/yarn.lock > ./tmp/yarn.lock
 if ! diff -q ./src/yarn.lock ./tmp/yarn.lock > /dev/null 2>&1; then
   echo "Saving yarn.lock"
-  cp /tmp/yarn.lock ./src/yarn.lock
+  cp ./tmp/yarn.lock ./src/yarn.lock
 fi
