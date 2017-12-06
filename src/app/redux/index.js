8b820a7212a6965073f2b1f59ba5e42a2b21c503
Richard Palmer <rich@rdjpalmer.com>
2017-12-06 17:33:58 +0000

    Add meta title template

--- src/app/redux/index.js
+++ src/app/redux/index.js
@@ -7,10 +7,9 @@ const style = require('./style.css')
 
 const ReduxRoot = props => (
   <div className={classnames(props.className, css(style.root))}>
-    <Helmet>
-      <meta charSet='utf-8' />
-      <title>nudj</title>
-      <meta name='viewport' content='width=device-width, initial-scale=1' />
+    <Helmet titleTemplate="%s | nudj" defaultTitle="nudj">
+      <meta charSet="utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
     </Helmet>
     {props.children}
   </div>
