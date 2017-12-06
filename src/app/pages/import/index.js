231c5ae35bc5a29fdd363e1e168184bf04289efe
Richard Palmer <rich@rdjpalmer.com>
2017-12-06 11:07:48 +0000

    Move network URL evaluation to be at initialisation time

--- src/app/pages/import/index.js
+++ src/app/pages/import/index.js
@@ -11,56 +11,54 @@ const style = require('./style.css')
 
 const getUrl = network => `/setup-network/${network}`
 
-const ImportPage = () => {
-  const networks = {
-    linkedin: {
-      url: getUrl('linkedin'),
-      label: 'LinkedIn'
-    },
-    facebook: {
-      url: getUrl('facebook'),
-      label: 'Facebook'
-    },
-    google: {
-      url: getUrl('google'),
-      label: 'Google'
-    }
+const NETWORKS = {
+  linkedin: {
+    url: getUrl('linkedin'),
+    label: 'LinkedIn'
+  },
+  facebook: {
+    url: getUrl('facebook'),
+    label: 'Facebook'
+  },
+  google: {
+    url: getUrl('google'),
+    label: 'Google'
   }
+}
 
-  return (
-    <div className={css(sharedStyle.root)}>
-      <Helmet>
-        <title>nudj - Choose a network</title>
-      </Helmet>
-      <div className={css(sharedStyle.wrapper)}>
-        <div className={css(sharedStyle.header)}>
-          <Text element="div" size="largeIi" style={sharedStyle.heading}>
-            Choose a network
-          </Text>
-          <Text element="p" style={sharedStyle.subheading}>
-            We recommend choosing the network where you feel the best
-            recommendations will come from based on your company and the roles
-            your hiring for.
-          </Text>
-        </div>
-        <div className={css(sharedStyle.body, style.buttonGroup)}>
-          <ButtonLink
-            style={style.button}
-            href={networks.linkedin.url}
-            volume="cheer"
-          >
-            {networks.linkedin.label}
-          </ButtonLink>
-          <ButtonLink style={style.button} href={networks.facebook.url}>
-            {networks.facebook.label} <Text size="smallI">- coming soon</Text>
-          </ButtonLink>
-          <ButtonLink style={style.button} href={networks.google.url}>
-            {networks.google.label} <Text size="smallI">- coming soon</Text>
-          </ButtonLink>
-        </div>
+const ImportPage = () => (
+  <div className={css(sharedStyle.root)}>
+    <Helmet>
+      <title>nudj - Choose a network</title>
+    </Helmet>
+    <div className={css(sharedStyle.wrapper)}>
+      <div className={css(sharedStyle.header)}>
+        <Text element="div" size="largeIi" style={sharedStyle.heading}>
+          Choose a network
+        </Text>
+        <Text element="p" style={sharedStyle.subheading}>
+          We recommend choosing the network where you feel the best
+          recommendations will come from based on your company and the roles
+          your hiring for.
+        </Text>
+      </div>
+      <div className={css(sharedStyle.body, style.buttonGroup)}>
+        <ButtonLink
+          style={style.button}
+          href={NETWORKS.linkedin.url}
+          volume="cheer"
+        >
+          {NETWORKS.linkedin.label}
+        </ButtonLink>
+        <ButtonLink style={style.button} href={NETWORKS.facebook.url}>
+          {NETWORKS.facebook.label} <Text size="smallI">- coming soon</Text>
+        </ButtonLink>
+        <ButtonLink style={style.button} href={NETWORKS.google.url}>
+          {NETWORKS.google.label} <Text size="smallI">- coming soon</Text>
+        </ButtonLink>
       </div>
     </div>
-  )
-}
+  </div>
+)
 
 module.exports = ImportPage
