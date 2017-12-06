f53b8df9c4b22de946b3c3b8fdecd2a7e86a6a20
Richard Palmer <rich@rdjpalmer.com>
2017-12-06 11:06:49 +0000

    Remove unused type declarations

--- src/flow-typed/index.js
+++ src/flow-typed/index.js
@@ -3,30 +3,3 @@ declare module StyleFile {
 }
 
 declare type ID = string | number
-
-declare type Hirer = {
-  onboarded: boolean
-}
-
-declare type User = {
-  company?: string,
-  created: Date,
-  email?: string,
-  firstName?: string,
-  id: ID,
-  lastName?: string,
-  status?: string,
-  title?: string,
-  type?: string,
-  modified: Date,
-  url?: string,
-  applications?: Array<mixed>,
-  externalMessages?: Array<mixed>,
-  hirer: Hirer,
-  recommendations?: Array<mixed>,
-  referrals?: Array<mixed>,
-  tasks?: Array<mixed>,
-  incompleteTaskCount?: number,
-  connections?: Array<mixed>,
-  formerEmployers?: Array<mixed>
-}
