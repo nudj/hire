ae1a77a4ecb58656db2c43a251c97451457f0827
Timothy Robinson <timmyrobby@gmail.com>
2017-12-08 10:24:05 +0000

    Update using new sharedStyles

--- src/app/pages/survey/style.css.js
+++ src/app/pages/survey/style.css.js
@@ -1,21 +1,12 @@
 const { StyleSheet, sizes, colors } = require('@nudj/components/lib/css')
-const { centerPageAction } = require('../../lib/css/breakpoints')
 
 const styleSheet = StyleSheet.create({
-  button: {
-    flexGrow: 1,
-    width: '100%',
-    [`@media(${centerPageAction.full})`]: {
-      flexGrow: 'inherit',
-      marginLeft: sizes.smallIi,
-      marginRight: sizes.smallIi,
-      paddingLeft: sizes.largeIii,
-      paddingRight: sizes.largeIii,
-      width: 'auto'
-    }
-  },
   companyName: {
     color: colors.midRed
+  },
+  startButton: {
+    paddingLeft: sizes.largeIi,
+    paddingRight: sizes.largeIi
   }
 })
 
