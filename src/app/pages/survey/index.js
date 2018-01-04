4a2f947cb6a757229e923e8fac7d0a0355815a24
Richard Palmer <rich@rdjpalmer.com>
2018-01-04 11:38:38 +0000

    Remove `/onboarding` from survey URL

--- src/app/pages/survey/index.js
+++ src/app/pages/survey/index.js
@@ -41,7 +41,6 @@ const SurveyPage = (props: SurveyProps) => {
   const survey = get(company, 'survey')
   const initialSection = get(survey, 'sections[0]')
   const initialQuestion = get(initialSection, 'questions[0]')
-  const onboardingSegment = get(user, 'hirer.onboarded') ? '' : '/onboarding'
 
   return (
     <div className={css(sharedStyle.root)}>
@@ -50,11 +49,7 @@ const SurveyPage = (props: SurveyProps) => {
       </Helmet>
       <div className={css(sharedStyle.wrapper)}>
         <div className={css(sharedStyle.header)}>
-          <Text
-            element='div'
-            size='largeIi'
-            style={sharedStyle.heading}
-          >
+          <Text element='div' size='largeIi' style={sharedStyle.heading}>
             Finding awesome people to join{' '}
             <span className={css(sharedStyle.headingHighlight)}>
               {get(company, 'name', '')}
@@ -66,18 +61,18 @@ const SurveyPage = (props: SurveyProps) => {
             your network.
           </Text>
           <Text element='p' style={sharedStyle.subheading}>
-            To help jog your memory and uncover those gems, we&#39;re now going to
-            ask you a series of questions. All you have to do is select from the
-            contacts you&#39;ve just uploaded who you feel is most relevant.
+            To help jog your memory and uncover those gems, we&#39;re now going
+            to ask you a series of questions. All you have to do is select from
+            the contacts you&#39;ve just uploaded who you feel is most relevant.
           </Text>
         </div>
         <div className={css(sharedStyle.body, sharedStyle.actions)}>
           <ButtonLink
             style={sharedStyle.action}
             volume='cheer'
-            href={`${onboardingSegment}/surveys/${survey.slug}/sections/${
-              initialSection.id
-            }/${initialQuestion.type}/${initialQuestion.id}`}
+            href={`/surveys/${survey.slug}/sections/${initialSection.id}/${
+              initialQuestion.type
+            }/${initialQuestion.id}`}
           >
             Start
           </ButtonLink>
