3e81af40c45b5605c5bdfc2d821e2cf41eccd35a
Nick Collings <nick@collingo.com>
2018-01-19 10:47:12 +0000

    Add fixes for errors/bugs/incompatabilities after testing hire with Arango (#267)

--- src/app/pages/survey-question/getNextSurveyUri/index.js
+++ src/app/pages/survey-question/getNextSurveyUri/index.js
@@ -9,18 +9,18 @@ function getNextSurveyUri (survey: Survey) {
     const section = get(survey, 'section')
     const allSections = get(survey, 'sections', [])
     const question = get(section, 'question')
-    const allQuestions = flatten(allSections.map(section => section.questions))
-    const questionIndex = findIndex(allQuestions, { id: question.id })
-    const nextQuestion = section.questions[questionIndex + 1]
+    const allQuestions = flatten(allSections.map(section => get(section, 'questions')))
+    const questionIndex = findIndex(allQuestions, { id: get(question, 'id') })
+    const nextQuestion = get(section, `questions[${questionIndex + 1}]`)
     const baseURL = `/surveys/${get(survey, 'slug')}`
 
     if (nextQuestion) {
-      const type = nextQuestion.type.toLowerCase()
+      const type = get(nextQuestion, 'type', '').toLowerCase()
       return `${baseURL}/sections/${section.id}/${type}/${nextQuestion.id}`
     }
-    const sectionIndex = findIndex(allSections, { id: section.id })
+    const sectionIndex = findIndex(allSections, { id: get(section, 'id') })
     const nextSection = allSections[sectionIndex + 1]
-    return nextSection ? `${baseURL}/sections/${nextSection.id}` : `${baseURL}/complete`
+    return nextSection ? `${baseURL}/sections/${get(nextSection, 'id')}` : `${baseURL}/complete`
   } catch (error) {
     console.error(error)
     return null
