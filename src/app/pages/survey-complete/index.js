576633c78ce2072409a64c41fb9dd3b5d03f4225
Richard Palmer <rich@rdjpalmer.com>
2018-01-24 18:16:29 +0000

    Refactor survey complete page to use wizard styles instead of shared.css

--- src/app/pages/survey-complete/index.js
+++ src/app/pages/survey-complete/index.js
@@ -7,14 +7,22 @@ const URLSearchParams = require('url-search-params')
 
 const { Modal, Text } = require('@nudj/components')
 const { css } = require('@nudj/components/lib/css')
+const m = require('@nudj/components/lib/css/modifiers.css')
 
 const style = require('./style.css')
-const sharedStyle = require('../shared.css')
 
 const ListRecommendations = require('../../components/list-recommendations')
 const ButtonLink = require('../../components/button-link')
 const Layout = require('../../components/app-layout')
 const EmailAuthForm = require('../../components/email-authentication-form')
+const {
+  Wrapper,
+  Section,
+  Heading,
+  P,
+  Footer,
+  styleSheet: wizardStyles,
+} = require('../../components/wizard')
 
 const getRecommendationCountString = recommendationCount => {
   if (recommendationCount === 1) return `${recommendationCount} person`
@@ -50,32 +58,32 @@ const ViewRecommendationsPage = (props: ViewRecommendationsProps) => {
   return (
     <Layout
       {...props}
-      styleSheet={{root: sharedStyle.root}}
+      styleSheet={{root: wizardStyles.root}}
       title='Part 3 - Send nudjes'
     >
       <Helmet>
         <title>View recommendations</title>
       </Helmet>
       {connections.length > 0 ? (
-        <div className={css(sharedStyle.wrapper)}>
-          <div className={css(sharedStyle.header)}>
-            <Text element='div' size='largeIi' style={[sharedStyle.heading, sharedStyle.headingPrimary]}>
+        <Wrapper>
+          <Section padding>
+            <Heading>
               You’ve uncovered{' '}
-              <span className={css(sharedStyle.headingHighlight)}>
+              <span className={css(m.fgMidRed)}>
                 {getRecommendationCountString(connections.length)}
               </span>{' '}
               worth nudj’ing from within your network
-            </Text>
-            <Text element='p' style={sharedStyle.subheading}>
+            </Heading>
+            <P>
               Now choose someone you’d like to send a nudj request to.
-            </Text>
-          </div>
-          <div className={css(sharedStyle.body, sharedStyle.cardMedium)}>
+            </P>
+          </Section>
+          <Section padding width="regular">
             <ListRecommendations
               recommendations={connections}
               getHref={getRecommendationHref}
             />
-          </div>
+          </Section>
           <Modal isOpen={!!selectedContactId} style={style.modalWindow}>
             <EmailAuthForm
               csrfToken={csrfToken}
@@ -83,28 +91,29 @@ const ViewRecommendationsPage = (props: ViewRecommendationsProps) => {
               method='post'
             />
           </Modal>
-        </div>
+        </Wrapper>
       ) : (
-        <div className={css(sharedStyle.wrapper)}>
-          <div className={css(sharedStyle.header)}>
-            <Text element='div' size='largeIi' style={[sharedStyle.heading, sharedStyle.headingPrimary]}>
+        <Wrapper>
+          <Section padding>
+            <Heading>
               You haven&#39;t found anyone worth nudj&#39;ing within your network
-            </Text>
-            <Text element='p' style={sharedStyle.subheading}>
+            </Heading>
+            <P>
               We suggest taking the survey again, only this time try to identify
               people who could give you good recommendations, not neccessarily
               those you&#39;d hire.
-            </Text>
-          </div>
-          <div className={css(sharedStyle.body)}>
+            </P>
+          </Section>
+          <Section padding>
             <ButtonLink
+              style={wizardStyles.action}
               href={`/surveys/${get(user, 'hirer.company.survey.slug', '')}`}
               volume='cheer'
             >
               Take survey again
             </ButtonLink>
-          </div>
-        </div>
+          </Section>
+        </Wrapper>
       )}
     </Layout>
   )
