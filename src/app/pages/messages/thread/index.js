da8ca9a74b013d010875f20f3c0ad8e5bd29b85e
Richard Palmer <rich@rdjpalmer.com>
2018-01-25 11:49:18 +0000

    fixup! Refactor messages/thread page to not rely on shared.css

--- src/app/pages/messages/thread/index.js
+++ src/app/pages/messages/thread/index.js
@@ -16,9 +16,9 @@ const m = require('@nudj/components/lib/css/modifiers.css')
 const { emailPreferences } = require('../../../lib/constants')
 const Layout = require('../../../components/app-layout')
 const ThreadItem = require('../../../components/email')
-const Wrapper = require('../../components/wrapper')
-const Section = require('../../components/section')
-const { Heading, P } = require('../../components/app')
+const Wrapper = require('../../../components/wrapper')
+const Section = require('../../../components/section')
+const { Heading, P } = require('../../../components/app')
 const style = require('./style.css')
 
 const MessageThreadPage = props => {
@@ -42,40 +42,41 @@ const MessageThreadPage = props => {
         <title>{subject}</title>
       </Helmet>
       <Wrapper>
-        <Section>
-        <Card style={m.pa0}>
-          <div className={css(style.threadSection)}>
-            <Text style={style.threadSubject} element='div' size='largeI'>
-              {subject}
-            </Text>
-            <Text style={style.threadName} element='div' size='smallI'>
-              {recipient.firstName} {recipient.lastName}
-            </Text>
-            <Text style={style.threadEmail} element='div' size='smallIi'>
-              {recipient.email}
-            </Text>
-          </div>
-          {fullThread.length > 0 &&
-            fullThread.map(message => (
-              <div className={css(style.threadSection)} key={message.id}>
-                <ThreadItem
-                  from={`${message.from.firstName} ${message.from.lastName} <${
-                    message.from.email
-                  }>`}
-                  body={message.body}
-                  date={format(message.date, 'DD MMM YY HH:mm')}
-                />
-              </div>
-            ))}
-          <form className={css(style.threadSection)} method='post'>
-            <Textarea name='body' placeholder='Write your message here...' />
-            <input name='_csrf' value={csrfToken} type='hidden' />
-            <Button style={style.replyButton} type='submit' volume='cheer'>
-              Reply
-            </Button>
-          </form>
-        </Card>
-      </div>
+        <Section width="largeI">
+          <Card style={m.pa0}>
+            <div className={css(style.threadSection)}>
+              <Text style={style.threadSubject} element='div' size='largeI'>
+                {subject}
+              </Text>
+              <Text style={style.threadName} element='div' size='smallI'>
+                {recipient.firstName} {recipient.lastName}
+              </Text>
+              <Text style={style.threadEmail} element='div' size='smallIi'>
+                {recipient.email}
+              </Text>
+            </div>
+            {fullThread.length > 0 &&
+              fullThread.map(message => (
+                <div className={css(style.threadSection)} key={message.id}>
+                  <ThreadItem
+                    from={`${message.from.firstName} ${message.from.lastName} <${
+                      message.from.email
+                    }>`}
+                    body={message.body}
+                    date={format(message.date, 'DD MMM YY HH:mm')}
+                  />
+                </div>
+              ))}
+            <form className={css(style.threadSection)} method='post'>
+              <Textarea name='body' placeholder='Write your message here...' />
+              <input name='_csrf' value={csrfToken} type='hidden' />
+              <Button style={style.replyButton} type='submit' volume='cheer'>
+                Reply
+              </Button>
+            </form>
+          </Card>
+        </Section>
+      </Wrapper>
     </Layout>
   )
 }
