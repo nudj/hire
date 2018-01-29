8b394f460758693b0a341c60e7f708f8480be962
Richard Palmer <rich@rdjpalmer.com>
2018-01-29 14:26:32 +0000

    Fix recommendations page rendering duplicate survey headings

--- src/app/pages/recommendations/index.js
+++ src/app/pages/recommendations/index.js
@@ -2,6 +2,8 @@
 const React = require('react')
 const { Helmet } = require('react-helmet')
 const get = require('lodash/get')
+const uniqBy = require('lodash/uniqBy')
+const flatten = require('lodash/flatten')
 
 const mss = require('@nudj/components/lib/css/modifiers.css')
 
@@ -13,8 +15,22 @@ const Section = require('../../components/section')
 const { Heading } = require('../../components/app')
 
 const RecommendationsPage = (props: Object) => {
-  const answers = get(props, 'surveyAnswers', [])
+  const surveyAnswers = get(props, 'surveyAnswers', [])
   const getRecommendationHref = ({id}) => `/messages/new/${id}`
+
+  const surveysMap = surveyAnswers.reduce((surveys, answer) => {
+    const survey = answer.surveyQuestion.surveySection.survey
+    const answers = get(surveys, `[${survey.slug}].answers`, [])
+    answers.push(answer)
+
+    return {
+      ...surveys,
+      [survey.slug]: {
+        ...survey,
+        answers
+      }
+    }
+  }, {})
 
   return (
     <Layout {...props}>
@@ -24,17 +40,22 @@ const RecommendationsPage = (props: Object) => {
       <Main>
         <Section padding width='regular' style={mss.left}>
           {
-            answers.map(answer => {
-              const id = get(answer, 'id')
-              const surveyTitle = get(answer, 'surveyQuestion.surveySection.survey.introTitle')
+            Object.keys(surveysMap).map((surveySlug) => {
+              const survey = surveysMap[surveySlug]
+              const { introTitle, answers } = survey
+
+              const connections = uniqBy(
+                flatten(answers.map(answer => answer.connections)),
+                'id'
+              )
 
               return (
-                <div key={id}>
+                <div key={survey}>
                   <Heading level={2}>
-                    { surveyTitle }
+                    { introTitle }
                   </Heading>
                   <ListRecommendations
-                    recommendations={answer.connections}
+                    recommendations={connections}
                     getHref={getRecommendationHref}
                   />
                 </div>
