4e73458c1f60790157ff5cb1375676da16279bbd
Richard Palmer <rich@rdjpalmer.com>
2018-01-29 14:32:18 +0000

    Standard fix

--- src/app/pages/survey-question/actions.js
+++ src/app/pages/survey-question/actions.js
@@ -132,7 +132,6 @@ module.exports.saveSurveyAnswers = surveyQuestion => async (dispatch, getState) 
     newSelectedConnections = uniq(newSelectedConnections.concat(savedConnections))
   }
 
-
   try {
     await dispatch(
       actions.app.postData(
--- src/app/pages/survey-question/reducer.js
+++ src/app/pages/survey-question/reducer.js
@@ -77,7 +77,7 @@ const resetConnectionsQuestionAnswers = (state, action) => {
     connections: [],
     searchQuery: null,
     newConnection: {},
-    connectionsChanged: false,
+    connectionsChanged: false
   }
 }
 
