546356c97564aad9ad976f82cb5f36896568c3ce
Richard Palmer <rich@rdjpalmer.com>
2018-01-29 16:58:30 +0000

    WIP

---
+++ src/app/components/staged-loader/index.js
@@ -0,0 +1,55 @@
+const React = require('react')
+const { css } = require('@nudj/components/lib/css')
+const { Text } = require('@nudj/components')
+
+const style = require('./style.css')
+
+class Loader extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      thresholdReached: false,
+      interval: null,
+      timeout: null
+    }
+  }
+
+  componentDidMount () {
+    const timeout = setTimeout(() => {
+      this.setState({ thresholdReached: true })
+    }, this.props.threshold)
+    this.setState({ timeout })
+  }
+
+  componentWillUnmount () {
+    if (this.state.timeout) {
+      clearInterval(this.state.timeout)
+    }
+  }
+
+  render () {
+    const { thresholdReached } = this.state
+    const { thresholdMessage, ellipsis, initialMessage } = this.props
+    const message = thresholdReached ? thresholdMessage : initialMessage
+
+    return (
+      <div className={css(style.root)}>
+        <div className={css(style.body)}>
+          <div className={css(style.spinner)} />
+          <Text element='div' size='largeI' style={style.message}>
+            {message}
+            {ellipsis && (
+              <span className={css(style.ellipsis)}>
+                <div className={css(style.ellipsisDot, style.dotOne)} />
+                <div className={css(style.ellipsisDot, style.dotTwo)} />
+                <div className={css(style.ellipsisDot)} />
+              </span>
+            )}
+          </Text>
+        </div>
+      </div>
+    )
+  }
+}
+
+module.exports = Loader
