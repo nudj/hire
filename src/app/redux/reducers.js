0ee7c687bacc985fa4a9f2835a3a3e9ac3487891
Timothy Robinson <timmyrobby@gmail.com>
2018-05-01 10:22:08 +0100

    minor: Add invite page reducer to redux reducers

--- src/app/redux/reducers.js
+++ src/app/redux/reducers.js
@@ -2,10 +2,12 @@ const surveyQuestionPage = require('../pages/survey-question/reducer')
 const uploadLinkedinConnectionsPage = require('../pages/setup-linkedin/reducer')
 const composeMessage = require('../pages/messages/new/reducer')
 const contactsPage = require('../pages/contacts/reducer')
+const invitePage = require('../pages/invite/reducer')
 
 module.exports = {
   surveyQuestionPage,
   uploadLinkedinConnectionsPage,
   contactsPage,
+  invitePage,
   composeMessage
 }
