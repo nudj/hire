099a0fecb3683cc816136ef7fda5e81762dc84e7
Timothy Robinson <timmyrobby@gmail.com>
2018-05-02 09:31:50 +0100

    minor: Add form to invite-team page

--- src/app/pages/invite-team/style.css.js
+++ src/app/pages/invite-team/style.css.js
@@ -1,50 +1,14 @@
-const { StyleSheet, colors, sizes } = require('@nudj/components/lib/css')
+const { StyleSheet, sizes } = require('@nudj/components/lib/css')
+const { wizardAction } = require('../../lib/css/breakpoints')
 
-const breakpoint = '56.25rem'
-
-const styleSheet = StyleSheet.create({
-  card: {
-    height: '100%'
-  },
-  list: {
-    listStyleType: 'none',
-    marginTop: 0,
-    marginBottom: 0,
-    paddingLeft: 0,
-    [`@media(min-width: ${breakpoint})`]: {
-      marginTop: `-${sizes.smallIi}`,
-      marginBottom: `-${sizes.smallIi}`,
-      alignItems: 'stretch'
+const stylesheet = StyleSheet.create({
+  sendInvitesButton: {
+    width: '100%',
+    marginTop: sizes.regular,
+    [`@media(${wizardAction.center})`]: {
+      width: 'auto'
     }
-  },
-  listItem: {
-    ':nth-child(1) ~ *': {
-      marginTop: sizes.largeI
-    },
-    [`@media(min-width: ${breakpoint})`]: {
-      paddingTop: sizes.smallIi,
-      paddingBottom: sizes.smallIi,
-      ':nth-child(1) ~ *': {
-        marginTop: 0
-      }
-    }
-  },
-  listItemContainer: {
-    maxWidth: '500px',
-    marginLeft: 'auto',
-    marginRight: 'auto',
-    textAlign: 'left'
-  },
-  listItemHeading: {
-    color: colors.primary,
-    marginTop: sizes.regular
-  },
-  listItemBody: {
-    marginTop: sizes.regular
-  },
-  button: {
-    marginTop: sizes.largeIi
   }
 })
 
-module.exports = styleSheet
+module.exports = stylesheet
