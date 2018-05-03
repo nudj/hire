c6745052e9bc14d717450de415d5568bba13ea41
Timothy Robinson <timmyrobby@gmail.com>
2018-05-03 11:43:00 +0100

    patch: Remove `ensureOnboarded` check from invite-team router

--- src/app/pages/invite-team/router.js
+++ src/app/pages/invite-team/router.js
@@ -1,14 +1,13 @@
 const createRouter = require('@nudj/framework/router')
 
 const fetchers = require('./fetchers')
-const { ensureOnboarded } = require('../../lib/middleware')
 
 const Router = ({ ensureLoggedIn, respondWithGql }) => {
   const router = createRouter()
   router.use(ensureLoggedIn)
 
-  router.getHandlers('/invite-team', ensureOnboarded, respondWithGql(fetchers.get))
-  router.postHandlers('/invite-team', ensureOnboarded, respondWithGql(fetchers.post))
+  router.getHandlers('/invite-team', ensureLoggedIn, respondWithGql(fetchers.get))
+  router.postHandlers('/invite-team', ensureLoggedIn, respondWithGql(fetchers.post))
 
   return router
 }
