5ffa89ba1c725cc85f1e483f2a904a7121db00a3
Timothy Robinson <timmyrobby@gmail.com>
2018-06-06 12:12:46 +0100

    minor: Add intercom tracking to companies that sign up

--- src/app/pages/setup-company/fetchers.js
+++ src/app/pages/setup-company/fetchers.js
@@ -1,7 +1,53 @@
 const { Redirect } = require('@nudj/framework/errors')
+const { intercom } = require('@nudj/library/analytics')
+const logger = require('@nudj/framework/logger')
 
 const { Global } = require('../../lib/graphql')
 const { createNotification } = require('../../lib')
+
+const triggerIntercomTracking = async (data, body) => {
+  try {
+    const { email } = data.user
+    const companyName = body.name
+
+    // Create company and fetch user
+    const [ company, user ] = await Promise.all([
+      intercom.companies.getOrCreate({
+        name: companyName,
+        company_id: companyName,
+        custom_attributes: {
+          location: body.location
+        }
+      }),
+      intercom.user.getBy({ email })
+    ])
+
+    // Add user to company and log company creation event
+    await Promise.all([
+      intercom.user.update({
+        user,
+        data: {
+          companies: [{
+            company_id: company.company_id,
+            id: company.id,
+            name: company.name
+          }]
+        }
+      }),
+      intercom.user.logEvent({
+        user,
+        event: {
+          name: 'created company',
+          metadata: {
+            company: company.name
+          }
+        }
+      })
+    ])
+  } catch (error) {
+    logger.log('error', error)
+  }
+}
 
 const get = () => {
   const gql = `
@@ -17,6 +63,7 @@ const post = ({ body }) => {
   const gql = `
     mutation setupCompany ($company: CompanyCreateInput!) {
       user {
+        email
         addCompanyAndAssignUserAsHirer(company: $company) {
           id
         }
@@ -29,7 +76,9 @@ const post = ({ body }) => {
       client: true
     }
   }
-  const respond = () => {
+  const respond = data => {
+    triggerIntercomTracking(data, body)
+
     const companyName = body.name
     throw new Redirect({
       url: '/setup-jobs',
