3cab48eb3cdedfa3fcdd6a8cd092c8e9ea648459
Timothy Robinson <timmyrobby@gmail.com>
2018-06-15 15:16:45 +0100

    minor: Remove network panel from team-member welcome page

--- src/app/pages/welcome/team-member.css.js
+++ src/app/pages/welcome/team-member.css.js
@@ -1,6 +1,6 @@
 const { StyleSheet, colors, sizes } = require('@nudj/components/lib/css')
 
-const breakpoint = '56.25rem'
+const breakpoint = '40rem'
 
 const styleSheet = StyleSheet.create({
   list: {
@@ -20,7 +20,7 @@ const styleSheet = StyleSheet.create({
       marginTop: sizes.largeI
     },
     [`@media(min-width: ${breakpoint})`]: {
-      flexBasis: '33.33333%',
+      flexBasis: '50%',
       paddingLeft: sizes.smallIi,
       paddingRight: sizes.smallIi,
       ':nth-child(1) ~ *': {
