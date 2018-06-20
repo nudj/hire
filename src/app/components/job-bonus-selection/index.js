c243212c32499307dc9fe6c5dd3450a06b50b685
Timothy Robinson <timmyrobby@gmail.com>
2018-06-20 15:22:42 +0100

    minor: Add `JobBonusSelection` component

---
+++ src/app/components/job-bonus-selection/index.js
@@ -0,0 +1,149 @@
+const React = require('react')
+
+const {
+  Icon,
+  IconInput,
+  IntegerInput,
+  Input,
+  RadioBlockGroup
+} = require('@nudj/components')
+const CardStyleSheet = require('@nudj/components/lib/components/card/style.css')
+const { css } = require('@nudj/components/lib/css')
+
+const { currencies, currencyKeys } = require('../../lib/constants')
+const style = require('./style.css')
+
+const currenciesArr = Object.keys(currencies)
+const presetBonusAmounts = ['500', '1000', '1500']
+
+const currencyRadioGroupStyles = [CardStyleSheet.root, style.cardInput]
+const presetValueRadioGroupStyles = [CardStyleSheet.root, style.cardInput, style.presetInput]
+const inputStyles = {
+  root: [CardStyleSheet.root, style.cardInput, style.valueInput],
+  input: style.input
+}
+const currencyRadioButtonStyleSheet = {
+  wrapper: style.currencyRadioWrapper
+}
+
+const presetRadioButtonStyleSheet = {
+  wrapper: style.presetRadioWrapper
+}
+
+const CurrencyBlock = ({ symbol, label, onClick }) => (
+  <span onClick={onClick} className={css(style.currencyBlock)}>
+    <span className={css(style.currencyIcon)}>
+      {symbol}
+    </span>
+    <span className={css(style.currencyLabel)}>
+      {label}
+    </span>
+  </span>
+)
+
+class JobBonusSelection extends React.Component {
+  componentDidUpdate () {
+    const { currencyValue } = this.props
+    if (currencyValue === currencyKeys.CUSTOM) {
+      this.customBonusInput.focus()
+    }
+  }
+
+  handleCurrencyChange = ({ value }) => {
+    const { presetValue, onCurrencyChange } = this.props
+
+    onCurrencyChange({
+      currencyValue: value,
+      inputValue: '',
+      presetValue: value === currencyKeys.CUSTOM ? '' : presetValue
+    })
+  }
+
+  handlePresetChange = ({ value }) => {
+    const { onPresetChange } = this.props
+
+    onPresetChange({
+      presetValue: value,
+      inputValue: ''
+    })
+  }
+
+  handleInputChange = ({ value }) => {
+    const { onInputChange } = this.props
+
+    onInputChange({
+      presetValue: '',
+      inputValue: value
+    })
+  }
+
+  render () {
+    const {
+      currencyValue,
+      presetValue,
+      inputValue
+    } = this.props
+
+    const selectedCurrency = currencies[currencyValue] || {}
+
+    return (
+      <div>
+        <RadioBlockGroup
+          name='currency'
+          value={currencyValue}
+          onChange={this.handleCurrencyChange}
+          styles={currencyRadioGroupStyles}
+          radioButtonStyleSheet={currencyRadioButtonStyleSheet}
+        >
+          {block => currenciesArr.map(currency => block({
+            key: currency,
+            id: currency,
+            value: currency,
+            label: (
+              <CurrencyBlock
+                symbol={<Icon name={currencies[currency].icon} />}
+                label={currency}
+              />
+            )
+          }))}
+        </RadioBlockGroup>
+        {currencyValue !== currencyKeys.CUSTOM && (
+          <RadioBlockGroup
+            name='value'
+            value={presetValue}
+            styles={presetValueRadioGroupStyles}
+            onChange={this.handlePresetChange}
+            radioButtonStyleSheet={presetRadioButtonStyleSheet}
+          >
+            {block => presetBonusAmounts.map(amount => block({
+              key: amount,
+              id: amount,
+              label: `${selectedCurrency.symbol}${amount}`,
+              value: amount
+            }))}
+          </RadioBlockGroup>
+        ) }
+        {currencyValue !== currencyKeys.CUSTOM ? (
+          <IntegerInput
+            Component={IconInput}
+            iconName={selectedCurrency.icon}
+            styleSheet={inputStyles}
+            placeholder='Other amount'
+            value={inputValue}
+            onChange={this.handleInputChange}
+          />
+        ) : (
+          <Input
+            styleSheet={inputStyles}
+            placeholder='Custom bonus'
+            ref={input => { this.customBonusInput = input }}
+            value={inputValue}
+            onChange={this.handleInputChange}
+          />
+        )}
+      </div>
+    )
+  }
+}
+
+module.exports = JobBonusSelection
