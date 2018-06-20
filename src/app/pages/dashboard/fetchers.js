d3a078fb7690ce59aa928e64b63323022fdb0443
Richard Palmer <rich@rdjpalmer.com>
2018-06-20 12:58:26 +0100

    Revert "major: Remove statistics from jobs page

--- src/app/pages/dashboard/fetchers.js
+++ src/app/pages/dashboard/fetchers.js
@@ -1,10 +1,30 @@
 const { cookies } = require('@nudj/library')
 const { jobStatuses } = require('../../lib/constants')
 const { Global } = require('../../lib/graphql')
+const {
+  format,
+  startOfWeek,
+  endOfWeek,
+  startOfMonth,
+  endOfMonth,
+  addWeeks,
+  addMonths
+} = require('date-fns')
 
-const get = ({ req, res, session }) => {
+const formatServerDate = (date) => format(date, 'YYYY-MM-DD')
+
+const get = ({ req, res, session, query }) => {
+  const { period } = query
+
   const gql = `
-    mutation GetJobs($jobStatus: JobStatus!, $userId: ID!) {
+    mutation GetDashboardStatistics(
+      $userId: ID!,
+      $dateFrom: DateTime,
+      $dateTo: DateTime,
+      $pastDateFrom: DateTime,
+      $pastDateTo: DateTime,
+      $jobStatus: JobStatus
+    ) {
       user {
         firstName
         emailPreference
@@ -19,7 +39,7 @@ const get = ({ req, res, session }) => {
             }
             name
             slug
-            jobs: jobsByFilters(filters: {status: $jobStatus}) {
+            jobs: jobsByFilters (filters: { status: $jobStatus }) {
               id
               title
               slug
@@ -28,6 +48,12 @@ const get = ({ req, res, session }) => {
               referral: getOrCreateReferralForUser(person: $userId) {
                 id
               }
+              applicationCount: applicationsCountByFilters(filters: { dateFrom: $dateFrom, dateTo: $dateTo})
+              referralCount: referralsCountByFilters(filters: { dateFrom: $dateFrom, dateTo: $dateTo})
+              viewCount: viewCountByFilters(filters: { dateFrom: $dateFrom, dateTo: $dateTo})
+              pastApplicationCount: applicationsCountByFilters(filters: { dateFrom: $pastDateFrom, dateTo: $pastDateTo})
+              pastReferralCount: referralsCountByFilters(filters: { dateFrom: $pastDateFrom, dateTo: $pastDateTo})
+              pastViewCount: viewCountByFilters(filters: { dateFrom: $pastDateFrom, dateTo: $pastDateTo})
             }
           }
         }
@@ -43,6 +69,30 @@ const get = ({ req, res, session }) => {
   const variables = {
     userId: session.userId,
     jobStatus: jobStatuses.PUBLISHED
+  }
+
+  const now = new Date()
+
+  if (period === 'week') {
+    const dateFrom = startOfWeek(now, { weekStartsOn: 1 })
+    const dateTo = endOfWeek(now, { weekStartsOn: 1 })
+    const pastDateFrom = addWeeks(dateFrom, -1)
+    const pastDateTo = addWeeks(dateTo, -1)
+
+    variables.dateFrom = formatServerDate(dateFrom)
+    variables.dateTo = formatServerDate(dateTo)
+    variables.pastDateFrom = formatServerDate(pastDateFrom)
+    variables.pastDateTo = formatServerDate(pastDateTo)
+  } else if (period === 'month') {
+    const dateFrom = startOfMonth(now)
+    const dateTo = endOfMonth(now)
+    const pastDateFrom = addMonths(dateFrom, -1)
+    const pastDateTo = addMonths(dateTo, -1)
+
+    variables.dateFrom = formatServerDate(dateFrom)
+    variables.dateTo = formatServerDate(dateTo)
+    variables.pastDateFrom = formatServerDate(pastDateFrom)
+    variables.pastDateTo = formatServerDate(pastDateTo)
   }
 
   const transformData = data => {
--- src/app/pages/dashboard/style.css.js
+++ src/app/pages/dashboard/style.css.js
@@ -4,6 +4,31 @@ const { modal } = require('../../lib/css/breakpoints')
 const breakpoint = '41.176470588rem'
 
 const styleSheet = StyleSheet.create({
+  statisticsList: {
+    [`@media(min-width: ${breakpoint})`]: {
+      display: 'flex',
+      marginLeft: `-${sizes.smallIi}`,
+      marginRight: `-${sizes.smallIi}`,
+      alignItems: 'stretch'
+    }
+  },
+  statisticItem: {
+    paddingTop: sizes.regular,
+    paddingRight: sizes.regular,
+    paddingBottom: sizes.regular,
+    paddingLeft: sizes.regular,
+    ':nth-child(1) ~ *': {
+      marginTop: sizes.largeI
+    },
+    [`@media(min-width: ${breakpoint})`]: {
+      flexBasis: '33.33333%',
+      marginLeft: sizes.smallIi,
+      marginRight: sizes.smallIi,
+      ':nth-child(1) ~ *': {
+        marginTop: 0
+      }
+    }
+  },
   jobCard: {
     paddingTop: 0,
     paddingRight: 0,
@@ -75,6 +100,7 @@ const styleSheet = StyleSheet.create({
     left: 'auto'
   },
   actions: {
+    marginTop: sizes.largeIii,
     textAlign: 'right'
   }
 })
