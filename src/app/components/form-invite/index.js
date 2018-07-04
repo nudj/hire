f2f5a51d73568ff92aef2b99ee0b927cadaa5f15
Timothy Robinson <timmyrobby@gmail.com>
2018-07-04 17:20:16 +0100

    minor: Update `form-invite` to accept `firstName` values

--- src/app/components/form-invite/index.js
+++ src/app/components/form-invite/index.js
@@ -3,6 +3,7 @@ const times = require('lodash/times')
 
 const { Input, Button, ScreenReadable } = require('@nudj/components')
 const { css } = require('@nudj/components/lib/css')
+const mss = require('@nudj/components/lib/css/modifiers.css')
 
 const style = require('./style.css')
 
@@ -12,27 +13,49 @@ const AddInviteForm = props => {
   return (
     <form className={css(style.form)} onSubmit={onSubmit}>
       <fieldset className={css(style.fieldset)}>
-        <legend className={css(style.fieldLabel)}>
-          Email addresses
-          <span className={css(style.asterisk)}>*</span>
-        </legend>
+        <div className={css(style.fieldLabel)}>
+          <div className={css(style.label, mss.mrReg)}>
+            Email addresses
+            <span className={css(style.asterisk)}>*</span>
+          </div>
+          <div className={css(style.label)}>
+            First name
+          </div>
+        </div>
         {
           times(fieldCount).map((_, index) => {
-            const key = `email${index}`
+            const key = `person${index}`
+            const emailKey = `${key}-email`
+            const nameKey = `${key}-firstName`
             return (
-              <div key={key}>
-                <ScreenReadable>
-                  <label htmlFor={key}>Email address {index + 1}</label>
-                </ScreenReadable>
-                <Input
-                  type='email'
-                  id={key}
-                  value={values[key] || ''}
-                  name={key}
-                  onChange={onChange}
-                  placeholder='name@example.com'
-                  styleSheet={{ root: style.field }}
-                />
+              <div className={css(style.flex)} key={emailKey}>
+                <div className={css(style.inputGroup, mss.mrReg)}>
+                  <ScreenReadable>
+                    <label htmlFor={emailKey}>Email address {index + 1}</label>
+                  </ScreenReadable>
+                  <Input
+                    type='email'
+                    id={emailKey}
+                    value={(values[key] && values[key].email) || ''}
+                    name={emailKey}
+                    onChange={onChange}
+                    placeholder='name@example.com'
+                    styleSheet={{ root: style.field }}
+                  />
+                </div>
+                <div className={css(style.inputGroup)}>
+                  <ScreenReadable>
+                    <label htmlFor={nameKey}>First name {index + 1}</label>
+                  </ScreenReadable>
+                  <Input
+                    id={nameKey}
+                    value={(values[key] && values[key].firstName) || ''}
+                    name={nameKey}
+                    onChange={onChange}
+                    placeholder='Name (optional)'
+                    styleSheet={{ root: style.field }}
+                  />
+                </div>
               </div>
             )
           })
