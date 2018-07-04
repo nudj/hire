bffc471778599f2dfeea92cf3658805e53b52efd
Timothy Robinson <timmyrobby@gmail.com>
2018-07-04 17:20:53 +0100

    minor: Update invite page to work with new form and resolver

--- src/app/pages/invite/actions.js
+++ src/app/pages/invite/actions.js
@@ -1,14 +1,15 @@
 const values = require('lodash/values')
 const actions = require('@nudj/framework/actions')
 
-const SET_FIELD_VALUE = 'INVITE_SET_FIELD_VALUE'
+const SET_NESTED_FIELD_VALUE = 'INVITE_SET_NESTED_FIELD_VALUE'
 const ADD_ADDITIONAL_FIELD = 'INVITE_ADD_ADDITIONAL_FIELD'
 const SUBMIT_INVITATIONS = 'INVITE_SUBMIT_INVITATIONS'
 const RESET_FORM = 'INVITE_RESET_FORM'
 const SHOW_LOADING = 'INVITE_SHOW_LOADING'
 
-const setFieldValue = (key, value) => ({
-  type: SET_FIELD_VALUE,
+const setNestedFieldValue = (person, key, value) => ({
+  type: SET_NESTED_FIELD_VALUE,
+  person,
   key,
   value
 })
@@ -28,9 +29,9 @@ const resetForm = () => ({
 const submitInvitations = () => async (dispatch, getState) => {
   const state = getState()
   const { fieldValues } = state.invitePage
-  const emailAddresses = values(fieldValues).filter(Boolean)
+  const members = values(fieldValues).filter(member => !!member.email)
 
-  if (!emailAddresses.length) {
+  if (!members.length) {
     return dispatch(actions.app.showNotification({
       type: 'error',
       message: 'Please add an email address'
@@ -42,20 +43,20 @@ const submitInvitations = () => async (dispatch, getState) => {
     actions.app.postData({
       url: '/invite',
       method: 'post',
-      data: { emailAddresses }
+      data: { members }
     })
   )
 }
 
 module.exports = {
   // constants
-  SET_FIELD_VALUE,
+  SET_NESTED_FIELD_VALUE,
   ADD_ADDITIONAL_FIELD,
   SUBMIT_INVITATIONS,
   RESET_FORM,
   SHOW_LOADING,
   // action creators
-  setFieldValue,
+  setNestedFieldValue,
   showLoading,
   submitInvitations,
   addAdditionalField
