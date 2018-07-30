db9a782574053735f9e84df8f9d4627d65f01bb3
Timothy Robinson <timmyrobby@gmail.com>
2018-07-30 17:23:07 +0100

    minor: Add `Small` component

---
+++ src/app/components/small/style.css.js
@@ -0,0 +1,11 @@
+const { StyleSheet, colors } = require('@nudj/components/lib/css')
+
+const styleSheet = StyleSheet.create({
+  root: {
+    color: colors.greyDarker,
+    opacity: 0.8,
+    display: 'inline-block'
+  }
+})
+
+module.exports = styleSheet
