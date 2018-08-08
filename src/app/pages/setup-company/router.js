402ecd24a1ef16a8ad9f51ad09d0a7c084aafee6
Timothy Robinson <timmyrobby@gmail.com>
2018-08-08 15:57:04 +0100

    minor: Add `ensureAdmin` to `/setup-company` router

--- src/app/pages/setup-company/router.js
+++ src/app/pages/setup-company/router.js
@@ -2,11 +2,15 @@ const createRouter = require('@nudj/framework/router')
 
 const fetchers = require('./fetchers')
 
-const { ensureNotOnboarded, ensureNoAccessRequestsPending } = require('../../lib/middleware')
+const {
+  ensureNotOnboarded,
+  ensureNoAccessRequestsPending,
+  ensureAdmin
+} = require('../../lib/middleware')
 
 const Router = ({ respondWithGql, ensureLoggedIn }) => {
   const router = createRouter()
-  router.use('/setup-company', ensureLoggedIn, ensureNoAccessRequestsPending, ensureNotOnboarded)
+  router.use('/setup-company', ensureLoggedIn, ensureNoAccessRequestsPending, ensureAdmin, ensureNotOnboarded)
 
   router.getHandlers('/setup-company', respondWithGql(fetchers.get))
   router.postHandlers('/setup-company', respondWithGql(fetchers.post))
