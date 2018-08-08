9679fd53bdca8e203a605f746f55a0b90fc01028
Richard Palmer <rich@rdjpalmer.com>
2018-08-08 12:49:59 +0100

    minor: Update welcome page design

--- src/app/pages/welcome/admin.css.js
+++ src/app/pages/welcome/admin.css.js
@@ -1,84 +1,60 @@
-const { StyleSheet, sizes } = require('@nudj/components/lib/css')
+const { StyleSheet, colors, sizes } = require('@nudj/components/lib/css')
 
-const breakpoint = '56.25rem'
+const breakpoint = '48.75rem'
 
 const styleSheet = StyleSheet.create({
-  card: {
-    overflow: 'hidden',
-    maxWidth: '36rem',
-    height: '100%',
-    margin: '0 auto',
+  body: {
+    display: 'flex'
+  },
+  image: {
+    display: 'none',
     [`@media(min-width: ${breakpoint})`]: {
-      maxWidth: '56rem'
+      display: 'block',
+      flexBasis: '50%',
+      flexGrow: 1,
+      marginRight: sizes.regular
     }
   },
-  list: {
-    listStyleType: 'none',
-    marginTop: 0,
+  card: {
+    textAlign: 'left',
+    [`@media(min-width: ${breakpoint})`]: {
+      flexBasis: '50%',
+      flexGrow: 1,
+      marginLeft: sizes.regular
+    }
+  },
+  footer: {
+    display: 'flex',
+    flexDirection: 'column',
+    marginTop: sizes.regular,
+    alignItems: 'flex-end'
+  },
+  help: {
+    textAlign: 'left',
+    [`@media(min-width: ${breakpoint})`]: {
+      width: '50%',
+      flexGrow: 1,
+      paddingLeft: sizes.regular
+    }
+  },
+  subheading: {
+    color: colors.primary,
+    ':nth-of-type(n + 2)': {
+      marginTop: sizes.regular
+    }
+  },
+  questions: {
+    paddingLeft: sizes.largeIi
+  },
+  questionsList: {
+    marginTop: sizes.smallIi,
     marginBottom: 0,
     paddingLeft: 0,
-    [`@media(min-width: ${breakpoint})`]: {
-      marginTop: `-${sizes.smallIi}`,
-      marginBottom: `-${sizes.smallIi}`,
-      alignItems: 'stretch'
-    }
+    listStyleType: 'none'
   },
-  listItem: {
-    marginTop: 0,
-    ':nth-child(1) ~ *': {
-      marginTop: sizes.regular
-    },
-    ':nth-child(1) *': {
-      ':after': {
-        backgroundImage: 'none',
-        [`@media(min-width: ${breakpoint})`]: {
-          width: '30rem',
-          height: '6.5rem',
-          left: '100%',
-          backgroundPosition: 'left',
-          backgroundImage: 'url(/assets/images/one-finger.svg)'
-        }
-      }
-    },
-    ':nth-child(2) *': {
-      ':after': {
-        backgroundImage: 'none',
-        [`@media(min-width: ${breakpoint})`]: {
-          width: '25rem',
-          height: '6.5rem',
-          right: 'calc(100% + 1.25rem)',
-          backgroundPosition: 'right',
-          backgroundImage: 'url(/assets/images/two-fingers.svg)'
-        }
-      }
-    },
-    ':nth-child(3) *': {
-      ':after': {
-        backgroundImage: 'none',
-        [`@media(min-width: ${breakpoint})`]: {
-          width: '17rem',
-          height: '24rem',
-          left: 'calc(100% - 8.75rem)',
-          backgroundPosition: 'left',
-          top: 'calc(50% - 6.25rem)',
-          backgroundImage: 'url(/assets/images/three-fingers.svg)'
-        }
-      }
-    }
-  },
-  listItemContainer: {
-    maxWidth: '31rem',
-    marginLeft: 'auto',
-    marginRight: 'auto',
-    textAlign: 'left',
-    position: 'relative',
-    ':after': {
-      content: '""',
-      display: 'block',
-      position: 'absolute',
-      top: 'calc(50% - 3rem)',
-      backgroundRepeat: 'no-repeat'
-    }
+  questionsListItem: {
+    display: 'inline',
+    marginRight: sizes.regular
   }
 })
 
--- src/app/pages/welcome/admin.js
+++ src/app/pages/welcome/admin.js
@@ -1,17 +1,13 @@
 const React = require('react')
 
-const { Text, Card } = require('@nudj/components')
+const { Link, Text, Card } = require('@nudj/components')
 const { css } = require('@nudj/components/lib/css')
 const mss = require('@nudj/components/lib/css/modifiers.css')
 
 const ButtonLink = require('../../components/button-link')
 const Main = require('../../components/main')
 const Section = require('../../components/section')
-const {
-  Heading,
-  Para,
-  styleSheet: wizardStyles
-} = require('../../components/wizard')
+const { Heading, Para } = require('../../components/wizard')
 
 const WelcomeAdmin = ({ style }) => (
   <Main>
@@ -23,54 +19,74 @@ const WelcomeAdmin = ({ style }) => (
         Get started with nudj and find out how you can source the best talent.
       </Para>
     </Section>
-    <Section>
+    <Section style={style.body} padding>
+      <img className={css(style.image)} src='/assets/images/review-the-best.svg' presentation />
       <Card style={style.card}>
-        <ul className={css(style.list)}>
-          <li className={css(style.listItem)}>
-            <div className={css(style.listItemContainer)}>
-              <Text nonsensitive element='h2' size='largeIi' style={mss.fgPrimary}>
-                <span className={css(mss.fgMidRed)}>1.</span>{' '}
-                Build a best-in-class referral program
-              </Text>
-              <Text nonsensitive style={mss.mtReg} element='p'>
-                A referral program that consistently delivers results - top talent and faster hires for less cash.
-              </Text>
-            </div>
-          </li>
-          <li className={css(style.listItem)}>
-            <div className={css(style.listItemContainer)}>
-              <Text nonsensitive element='h2' size='largeIi' style={mss.fgPrimary}>
-                <span className={css(mss.fgMidRed)}>2.</span>{' '}
-                Steal Google&apos;s magic
-              </Text>
-              <Text nonsensitive style={mss.mtReg} element='p'>
-                Use techniques pioneered by Google&apos;s hiring team to help your employees uncover those hidden gems from their networks.
-              </Text>
-            </div>
-          </li>
-          <li className={css(style.listItem)}>
-            <div className={css(style.listItemContainer)}>
-              <Text nonsensitive element='h2' size='largeIi' style={mss.fgPrimary}>
-                <span className={css(mss.fgMidRed)}>3.</span>{' '}
-                Hire like a hero
-              </Text>
-              <Text nonsensitive style={mss.mtReg} element='p'>
-                Track performance, manage applicants and make more referrals happen all from within the app.
-              </Text>
-            </div>
-          </li>
-        </ul>
+        <Text nonsensitive element='h2' size='largeI' style={style.subheading}>
+          Build a best-in-class referral program
+        </Text>
+        <Text nonsensitive style={mss.mtReg} element='p'>
+          A referral program that consistently delivers results - top talent and faster hires for less cash.
+        </Text>
+        <Text nonsensitive element='h2' size='largeI' style={style.subheading}>
+          Steal Google&apos;s magic
+        </Text>
+        <Text nonsensitive style={mss.mtReg} element='p'>
+          Use techniques pioneered by Google&apos;s hiring team to help your employees uncover those hidden gems from their networks.
+        </Text>
+        <Text nonsensitive element='h2' size='largeI' style={style.subheading}>
+          Hire like a hero
+        </Text>
+        <Text nonsensitive style={mss.mtReg} element='p'>
+          Track performance, manage applicants and make more referrals happen all from within the app.
+        </Text>
+        <ButtonLink
+          href='/'
+          style={mss.mtLgIi}
+          volume='cheer'
+        >
+          Explore nudj
+        </ButtonLink>
       </Card>
     </Section>
-    <Section padding>
-      <ButtonLink
-        nonsensitive
-        style={wizardStyles.action}
-        href='/setup-company'
-        volume='cheer'
-      >
-        Okay, I&apos;m ready
-      </ButtonLink>
+    <Section style={style.footer} padding>
+      <div className={css(style.help)}>
+        <Text
+          nonsensitive
+          element='h2'
+          size='regular'
+          style={[
+            mss.bold,
+            mss.fgPrimary
+          ]}
+        >
+          Need help?
+        </Text>
+        <ul className={css(style.questionsList)}>
+          <li className={css(style.questionsListItem)}>
+            <Link
+              nonsensitive
+              href='mailto:help@nudj.co'
+              id='open-intercom'
+              subtle
+              inline
+            >
+              Talk to a human
+            </Link>
+          </li>
+          <li className={css(style.questionsListItem)}>
+            <Link
+              href='https://help.nudj.co'
+              target='_blank'
+              nonsensitive
+              subtle
+              inline
+            >
+              FAQ
+            </Link>
+          </li>
+        </ul>
+      </div>
     </Section>
   </Main>
 )
