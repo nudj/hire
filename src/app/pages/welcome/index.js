4c51330bd7fa7a9847fc6141c112530a22fde176
Richard Palmer <rich@rdjpalmer.com>
2018-08-08 12:49:59 +0100

    minor: Update welcome page design

--- src/app/pages/welcome/index.js
+++ src/app/pages/welcome/index.js
@@ -1,86 +1,94 @@
 const React = require('react')
-const { Helmet } = require('react-helmet')
 
-const { Text, Card } = require('@nudj/components')
+const { Link, Text, Card } = require('@nudj/components')
 const { css } = require('@nudj/components/lib/css')
 const mss = require('@nudj/components/lib/css/modifiers.css')
 
 const ButtonLink = require('../../components/button-link')
 const Main = require('../../components/main')
 const Section = require('../../components/section')
-const Layout = require('../../components/app-layout')
-const {
-  Heading,
-  Para,
-  styleSheet: wizardStyles
-} = require('../../components/wizard')
-const style = require('./style.css')
+const { Heading, Para } = require('../../components/wizard')
 
-const WelcomePage = props => (
-  <Layout {...props}>
-    <Helmet>
-      <title>Welcome</title>
-    </Helmet>
-    <Main>
-      <Section padding>
-        <Heading nonsensitive>
-          Welcome to nudj!
-        </Heading>
-        <Para nonsensitive>
-          Get started with nudj and find out how you can source the best talent.
-        </Para>
-      </Section>
-      <Section>
-        <Card style={style.card}>
-          <ul className={css(style.list)}>
-            <li className={css(style.listItem)}>
-              <div className={css(style.listItemContainer)}>
-                <Text nonsensitive element='h2' size='largeIi' style={mss.fgPrimary}>
-                  <span className={css(mss.fgMidRed)}>1.</span>{' '}
-                  Build a best-in-class referral program
-                </Text>
-                <Text nonsensitive style={mss.mtReg} element='p'>
-                  A referral program that consistently delivers results - top talent and faster hires for less cash.
-                </Text>
-              </div>
-            </li>
-            <li className={css(style.listItem)}>
-              <div className={css(style.listItemContainer)}>
-                <Text nonsensitive element='h2' size='largeIi' style={mss.fgPrimary}>
-                  <span className={css(mss.fgMidRed)}>2.</span>{' '}
-                  Steal Google&apos;s magic
-                </Text>
-                <Text nonsensitive style={mss.mtReg} element='p'>
-                  Use techniques pioneered by Google&apos;s hiring team to help your employees uncover those hidden gems from their networks.
-                </Text>
-              </div>
-            </li>
-            <li className={css(style.listItem)}>
-              <div className={css(style.listItemContainer)}>
-                <Text nonsensitive element='h2' size='largeIi' style={mss.fgPrimary}>
-                  <span className={css(mss.fgMidRed)}>3.</span>{' '}
-                  Hire like a hero
-                </Text>
-                <Text nonsensitive style={mss.mtReg} element='p'>
-                  Track performance, manage applicants and make more referrals happen all from within the app.
-                </Text>
-              </div>
-            </li>
-          </ul>
-        </Card>
-      </Section>
-      <Section padding>
+const WelcomeAdmin = ({ style }) => (
+  <Main>
+    <Section padding>
+      <Heading nonsensitive>
+        Welcome to nudj!
+      </Heading>
+      <Para nonsensitive>
+        Get started with nudj and find out how you can source the best talent.
+      </Para>
+    </Section>
+    <Section style={style.body} padding>
+      <img className={css(style.image)} src='/assets/images/review-the-best.svg' presentation />
+      <Card style={style.card}>
+        <Text nonsensitive element='h2' size='largeI' style={style.subheading}>
+          Build a best-in-class referral program
+        </Text>
+        <Text nonsensitive style={mss.mtReg} element='p'>
+          A referral program that consistently delivers results - top talent and faster hires for less cash.
+        </Text>
+        <Text nonsensitive element='h2' size='largeI' style={style.subheading}>
+          Steal Google&apos;s magic
+        </Text>
+        <Text nonsensitive style={mss.mtReg} element='p'>
+          Use techniques pioneered by Google&apos;s hiring team to help your employees uncover those hidden gems from their networks.
+        </Text>
+        <Text nonsensitive element='h2' size='largeI' style={style.subheading}>
+          Hire like a hero
+        </Text>
+        <Text nonsensitive style={mss.mtReg} element='p'>
+          Track performance, manage applicants and make more referrals happen all from within the app.
+        </Text>
         <ButtonLink
-          nonsensitive
-          style={wizardStyles.action}
           href='/setup-company'
+          style={mss.mtLgIi}
           volume='cheer'
         >
-          Okay, I&apos;m ready
+          Explore nudj
         </ButtonLink>
-      </Section>
-    </Main>
-  </Layout>
+      </Card>
+    </Section>
+    <Section style={style.footer} padding>
+      <div className={css(style.help)}>
+        <Text
+          nonsensitive
+          element='h2'
+          size='regular'
+          style={[
+            mss.bold,
+            mss.fgPrimary
+          ]}
+        >
+          Need help?
+        </Text>
+        <ul className={css(style.questionsList)}>
+          <li className={css(style.questionsListItem)}>
+            <Link
+              nonsensitive
+              href='mailto:help@nudj.co'
+              id='open-intercom'
+              subtle
+              inline
+            >
+              Talk to a human
+            </Link>
+          </li>
+          <li className={css(style.questionsListItem)}>
+            <Link
+              href='https://help.nudj.co'
+              target='_blank'
+              nonsensitive
+              subtle
+              inline
+            >
+              FAQ
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </Section>
+  </Main>
 )
 
-module.exports = WelcomePage
+module.exports = WelcomeAdmin
