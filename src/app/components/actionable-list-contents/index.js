bba6106128bc2220cc51cb80826d4c4e0d24ca39
Timothy Robinson <timmyrobby@gmail.com>
2018-10-15 12:42:04 +0100

    minor: Add `ActionableListContents` component

---
+++ src/app/components/actionable-list-contents/index.js
@@ -0,0 +1,100 @@
+const React = require('react')
+const PropTypes = require('prop-types')
+let memoize = require('memoize-one')
+memoize = memoize.default || memoize
+
+const { Text } = require('@nudj/components')
+const { css, mergeStyleSheets } = require('@nudj/components/lib/css')
+const CustomPropTypes = require('@nudj/components/lib/helpers/prop-types')
+
+const defaultStyleSheet = require('./style.css')
+
+const ListAction = props => {
+  const {
+    style = {},
+    Component = 'button',
+    ...rest
+  } = props
+
+  return (
+    <Component
+      className={css({ ...defaultStyleSheet.action, ...style })}
+      {...rest}
+    />
+  )
+}
+
+class ActionableListContents extends React.Component {
+  static propTypes = {
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    children: PropTypes.func,
+    styleSheet: CustomPropTypes.style
+  }
+
+  getStyle = memoize(
+    (defaultStyleSheet, styleSheet) => mergeStyleSheets(defaultStyleSheet, styleSheet)
+  )
+
+  render () {
+    const {
+      title,
+      subtitle,
+      dataPoints,
+      children,
+      styleSheet
+    } = this.props
+
+    const style = this.getStyle(defaultStyleSheet, styleSheet)
+
+    return (
+      <div className={css(style.root)}>
+        <div className={css(style.body)}>
+          <div className={css(style.details)}>
+            <div className={css(style.titleContainer)}>
+              <Text element='div' size='largeI' style={style.title} nonsensitive>
+                {title}
+              </Text>
+            </div>
+            {subtitle && (
+              <Text element='span' size='smallI' style={style.location} nonsensitive>
+                {subtitle}
+              </Text>
+            )}
+          </div>
+          <dl className={css(style.statisticsList)}>
+            {dataPoints.map((data, i) => (
+              // Valid markup to have div inside dl
+              // https://developer.mozilla.org/en-US/docs/Web/HTML/Element/dl#Wrapping_name-value_groups_in_%3Cdiv%3E_elements
+              <div key={i} className={css(style.statisticItem)}>
+                <Text
+                  size='smallI'
+                  element='dt'
+                  style={style.statisticValue}
+                  nonsensitive
+                >
+                  {data.value}
+                </Text>
+                <Text
+                  size='smallI'
+                  element='dd'
+                  style={style.statisticLabel}
+                  nonsensitive
+                >
+                  {data.key}
+                </Text>
+              </div>
+            ))}
+          </dl>
+        </div>
+        {children && (
+          <div className={css(style.actions)}>
+            {children(ListAction)}
+          </div>
+        )}
+      </div>
+    )
+  }
+}
+
+module.exports = ActionableListContents
