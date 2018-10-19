9498f0aeb8c8424b81a6eaac42202f877da0c052
Timothy Robinson <timmyrobby@gmail.com>
2018-10-19 10:06:31 +0100

    minor: Move user hirer to "no team" state page

--- src/app/pages/team/index.js
+++ src/app/pages/team/index.js
@@ -13,91 +13,88 @@ const { Heading, Para } = require('../../components/app')
 const style = require('./style.css')
 
 const TeamPage = props => {
-  const userHirer = props.user.hirer
   const { hirers } = props.user.hirer.company
 
-  const hirersExcludingUser = hirers.filter(hirer => hirer.id !== userHirer.id)
   const hirersGroupedByType = groupBy(hirers, 'type')
   const hirerTypes = Object.keys(hirersGroupedByType).sort()
 
   return (
     <Layout {...props}>
-      {hirersExcludingUser.length ? (
-        <div>
-          <ActionBar>
-            {actionStyle => [
-              <ButtonLink
-                key='invite-button'
-                style={actionStyle}
-                volume='cheer'
-                to='/team/invite'
-                subtle
-              >
-                Invite teammates
-              </ButtonLink>
-            ]}
-          </ActionBar>
-          <div className={css(style.listContainer)}>
-            <List style={mss.mtReg}>
-              {ListItem => hirerTypes.map(hirerType => {
-                const hirers = hirersGroupedByType[hirerType]
+      <div>
+        <ActionBar>
+          {actionStyle => [
+            <ButtonLink
+              key='invite-button'
+              style={actionStyle}
+              volume='cheer'
+              to='/team/invite'
+              subtle
+            >
+              Invite teammates
+            </ButtonLink>
+          ]}
+        </ActionBar>
+        <div className={css(style.listContainer)}>
+          <List style={mss.mtReg}>
+            {ListItem => hirerTypes.map(hirerType => {
+              const hirers = hirersGroupedByType[hirerType]
 
-                if (!hirers.length) return null
+              if (!hirers.length) return null
 
-                return (
-                  <div key={hirerType}>
-                    <Heading level={2} size='smallIi' nonsensitive style={style.listDivider}>
-                      {`${startCase(hirerType.toLowerCase())}s`}
-                    </Heading>
-                    {hirers.map(hirer => (
-                      <ListItem key={hirer.id} joined>
-                        <a className={css(style.card)} href={`/team/${hirer.id}`}>
-                          <Align
-                            leftChildren={(
-                              <div>
-                                <div className={css(style.titleContainer)}>
-                                  <Text element='div' size='largeI' style={style.title} nonsensitive>
-                                    {`${hirer.person.firstName} ${hirer.person.lastName}`}
-                                  </Text>
-                                </div>
-                                <Text element='span' size='smallI' style={style.subtitle} nonsensitive>
-                                  {hirer.person.email}
+              return (
+                <div key={hirerType}>
+                  <Heading level={2} size='smallIi' nonsensitive style={style.listDivider}>
+                    {`${startCase(hirerType.toLowerCase())}s`}
+                  </Heading>
+                  {hirers.map(hirer => (
+                    <ListItem key={hirer.id} joined>
+                      <a className={css(style.card)} href={`/team/${hirer.id}`}>
+                        <Align
+                          leftChildren={(
+                            <div>
+                              <div className={css(style.titleContainer)}>
+                                <Text element='div' size='largeI' style={style.title} nonsensitive>
+                                  {`${hirer.person.firstName} ${hirer.person.lastName}`}
                                 </Text>
                               </div>
-                            )}
-                            rightChildren={(
-                              <Icon style={style.chevron} name='chevron' />
-                            )}
-                          />
-                        </a>
-                      </ListItem>
-                    ))}
-                  </div>
-                )
-              })}
-            </List>
-          </div>
+                              <Text element='span' size='smallI' style={style.subtitle} nonsensitive>
+                                {hirer.person.email}
+                              </Text>
+                            </div>
+                          )}
+                          rightChildren={(
+                            <Icon style={style.chevron} name='chevron' />
+                          )}
+                        />
+                      </a>
+                    </ListItem>
+                  ))}
+                </div>
+              )
+            })}
+          </List>
         </div>
-      ) : (
-        <Section style={[mss.center, mss.mtReg]}>
-          <Heading nonsensitive level={1} style={mss.fgPrimary}>
-            Add your team
-          </Heading>
-          <Para nonsensitive>
-            Be sure to invite your entire team to nudj. After all, many
-            hands make light work (and more referrals).
-          </Para>
-          <ButtonLink
-            nonsensitive
-            href='/team/invite'
-            style={mss.mtLgI}
-            volume='cheer'
-            subtle
-          >
-            Invite teammates
-          </ButtonLink>
-        </Section>
-      )}
+        {hirers.length === 1 && (
+          <Section style={[mss.center, mss.mtReg]}>
+            <Heading nonsensitive level={1} style={mss.fgPrimary}>
+              Add your team
+            </Heading>
+            <Para nonsensitive>
+              Be sure to invite your entire team to nudj. After all, many
+              hands make light work (and more referrals).
+            </Para>
+            <ButtonLink
+              nonsensitive
+              href='/team/invite'
+              style={mss.mtLgI}
+              volume='cheer'
+              subtle
+            >
+              Invite teammates
+            </ButtonLink>
+          </Section>
+        )}
+      </div>
     </Layout>
   )
 }
