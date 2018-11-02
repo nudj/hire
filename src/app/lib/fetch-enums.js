db419bb09dede2fa02b71d4560311d784fb23b10
Nick Collings <nick@collingo.com>
2018-11-02 15:06:31 +0000

    patch: Update fetch enums to operate over multiple types

--- src/app/lib/fetch-enums.js
+++ src/app/lib/fetch-enums.js
@@ -1,18 +1,31 @@
 const requestGQL = require('./requestGql')
+const mapValues = require('lodash/mapValues')
+
 const { createEnumMap } = require('../lib')
 
-const fetchEnums = async enumName => {
-  const response = await requestGQL(null, `
-    query fetchEnum ($enumName: String!) {
-      enumTypes: __type(name: $enumName) {
+const fetchEnums = async typeMap => {
+  const args = []
+  const variables = {}
+  const queries = []
+  Object.values(typeMap).forEach(type => {
+    args.push(`$${type}: String!`)
+    variables[type] = type
+    queries.push(`
+      ${type}: __type(name: $${type}) {
         values: enumValues {
           name
         }
       }
-    }
-  `, { enumName })
+    `)
+  })
 
-  return response.enumTypes && createEnumMap(response.enumTypes.values)
+  const data = await requestGQL(null, `
+    query fetchEnum (${args.join('\n')}) {
+      ${queries.join('')}
+    }
+  `, variables)
+
+  return mapValues(typeMap, type => data[type] && createEnumMap(data[type].values))
 }
 
 module.exports = fetchEnums
--- src/app/pages/referrals/fetchers.js
+++ src/app/pages/referrals/fetchers.js
@@ -1,25 +1,14 @@
 const { Global } = require('../../lib/graphql')
-const { createEnumMap } = require('../../lib')
+const fetchEnums = require('../../lib/fetch-enums')
 
 const get = async ({ requestGQL, session, query }) => {
-  const enumValues = await requestGQL({
-    gql: `
-      {
-        jobStatusTypes: __type(name: "JobStatus") {
-          values: enumValues {
-            name
-          }
-        }
-        hirerTypes: __type(name: "HirerType") {
-          values: enumValues {
-            name
-          }
-        }
-      }
-    `
+  const {
+    jobStatuses,
+    hirerTypes
+  } = await fetchEnums({
+    jobStatuses: 'JobStatus',
+    hirerTypes: 'HirerType'
   })
-  const jobStatusTypes = createEnumMap(enumValues.jobStatusTypes.values)
-  const hirerTypes = createEnumMap(enumValues.hirerTypes.values)
 
   const { parent } = query
   const gql = `
@@ -69,7 +58,7 @@ const get = async ({ requestGQL, session, query }) => {
   const variables = {
     parent,
     withParent: !!parent,
-    status: jobStatusTypes.PUBLISHED
+    status: jobStatuses.PUBLISHED
   }
   const transformData = data => ({
     ...data,
