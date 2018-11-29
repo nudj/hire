bf6c6c33b1cec28847232953677d8659f1197c42
Nick Collings <nick@collingo.com>
2018-11-29 17:24:33 +0000

    major: Add Discover Survey page, convert SurveyQuestion name to slug fetches

--- src/app/pages/survey/index.js
+++ src/app/pages/survey/index.js
@@ -1,25 +1,33 @@
 const React = require('react')
 const { Helmet } = require('react-helmet')
-const get = require('lodash/get')
-
+const { List, Text, Align, Link, Icon } = require('@nudj/components')
 const { css, mss } = require('@nudj/components/styles')
+const isNil = require('lodash/isNil')
 
-const analytics = require('../../lib/browser-analytics')
-const ButtonLink = require('../../components/button-link')
 const Layout = require('../../components/app-layout')
+const ButtonLink = require('../../components/button-link')
+const Dropdown = require('../../components/dropdown')
 const Main = require('../../components/main')
 const Section = require('../../components/section')
+const TitleCard = require('../../components/title-card')
+const { Heading } = require('../../components/app')
+const { fetchName } = require('../../lib')
+const analytics = require('../../lib/browser-analytics')
+
+const style = require('./style.css')
 const {
-  Heading,
-  Para,
+  Heading: WizardHeading,
+  Para: WizardPara,
   styleSheet: wizardStyles
 } = require('../../components/wizard')
 
 const SurveyPage = props => {
-  const { user } = props
-  const company = get(user, 'hirer.company')
-  const survey = get(company, 'survey')
-  const initialQuestion = get(survey, 'questions[0]', {})
+  const survey = props.user.hirer.company.survey
+  const questions = survey.questions
+  const title = survey.introTitle || 'Survey'
+  const intro = survey.introDescription || 'Intro text'
+  const emailPreferences = props.emailPreferences
+  const emailPreference = props.user.emailPreference || null
 
   const trackSurveyStart = () => {
     analytics.track({
@@ -31,54 +39,131 @@ const SurveyPage = props => {
     })
   }
 
+  // if any question is answered then they are all answered as there is a redirect in the fetcher when the survey is only partially complete
+  const isComplete = questions && questions[0] && questions[0].answer
+
+  const getRecommendationHref = ({ personId }) => {
+    return emailPreference !== emailPreferences.OTHER && !isNil(emailPreference)
+      ? `/messages/new/${personId}`
+      : `?id=${personId}`
+  }
+
   return (
     <Layout {...props}>
       <Helmet>
-        <title>Uncover hidden gems</title>
+        <title>{title}</title>
       </Helmet>
-      <Main>
-        <Section padding>
-          <Heading nonsensitive>
-            Explore your network to find your next teammate
-          </Heading>
-          <Para nonsensitive>
-            The people you need to hire are already in your network, to find them
-            you&apos;ve got to do a little digging.
-          </Para>
-          <Para nonsensitive>
-            Stealing a little of Google&apos;s wisdom, we&apos;ll run you through a few key
-            questions to help you uncover who you know beyond the usual suspects.
-          </Para>
-          <Para nonsensitive>
-            <em className={css(mss.i)}>Remember to think broadly and inclusively.</em>
-          </Para>
-        </Section>
-        <Section padding>
-          <ButtonLink
-            nonsensitive
-            style={wizardStyles.action}
-            volume='cheer'
-            href={`/surveys/${survey.slug}/questions/${initialQuestion.id}`}
-            onClick={trackSurveyStart}
+      {isComplete ? (
+        <Main>
+          <TitleCard
+            title={title}
+            titleRight={(
+              <Dropdown
+                id='survey'
+                header='Actions'
+                chevron
+              >
+                <Link
+                  href={`/surveys/${survey.slug}/questions/${questions[0].id}`}
+                  subtle
+                >
+                  Retake
+                </Link>
+              </Dropdown>
+            )}
           >
-            Start
-          </ButtonLink>
-        </Section>
-      </Main>
+            <Text element='p' style={style.descriptionParagraph}>
+              {intro}
+            </Text>
+          </TitleCard>
+
+          {questions.map(question => {
+            return (
+              <div key={question.id} className={css(style.listHeading)}>
+                <Heading
+                  id={question.id}
+                  level={2}
+                  style={style.heading}
+                  nonsensitive
+                >
+                  {question.title}
+                </Heading>
+                {!question.answer.connections.length ? (
+                  <Text style={style.spacer}>
+                    No answers given
+                  </Text>
+                ) : (
+                  <List style={[mss.mtReg, { textAlign: 'left' }]}>
+                    {ListItem => question.answer.connections.map(connection => {
+                      const personId = connection.person.id
+
+                      return (
+                        <ListItem key={connection.id} joined>
+                          <a className={css(style.card)} href={getRecommendationHref({ personId })}>
+                            <Align
+                              leftChildren={(
+                                <div>
+                                  <div className={css(style.titleContainer)}>
+                                    <Text element='div' size='largeI' style={style.title} nonsensitive>
+                                      {fetchName({
+                                        firstName: connection.firstName,
+                                        lastName: connection.lastName,
+                                        email: connection.person.email
+                                      })}
+                                    </Text>
+                                  </div>
+                                  <Text element='span' size='smallI' style={style.subtitle} nonsensitive>
+                                    {connection.person.email}
+                                  </Text>
+                                </div>
+                              )}
+                              rightChildren={(
+                                <Icon style={style.chevron} name='chevron' />
+                              )}
+                            />
+                          </a>
+                        </ListItem>
+                      )
+                    })}
+                  </List>
+                )}
+              </div>
+            )
+          })}
+        </Main>
+      ) : (
+        <Main>
+          <Section padding>
+            <WizardHeading nonsensitive>
+              Explore your network to find your next teammate
+            </WizardHeading>
+            <WizardPara nonsensitive>
+              The people you need to hire are already in your network, to find them
+              you&apos;ve got to do a little digging.
+            </WizardPara>
+            <WizardPara nonsensitive>
+              Stealing a little of Google&apos;s wisdom, we&apos;ll run you through a few key
+              questions to help you uncover who you know beyond the usual suspects.
+            </WizardPara>
+            <WizardPara nonsensitive>
+              <em className={css(mss.i)}>Remember to think broadly and inclusively.</em>
+            </WizardPara>
+          </Section>
+          <Section padding>
+            <ButtonLink
+              nonsensitive
+              style={wizardStyles.action}
+              volume='cheer'
+              href={`/surveys/${survey.slug}/questions/${questions[0].id}`}
+              onClick={trackSurveyStart}
+            >
+              Start
+            </ButtonLink>
+          </Section>
+        </Main>
+      )}
     </Layout>
   )
-}
-
-SurveyPage.defaultProps = {
-  user: {
-    hirer: {
-      company: {
-        survey: {
-          sections: []
-        }
-      }
-    }
-  }
 }
 
 module.exports = SurveyPage
---
+++ src/app/pages/survey/style.css.js
@@ -0,0 +1,82 @@
+const { StyleSheet, typography, sizes, colors } = require('@nudj/components/styles')
+
+const card = {
+  textAlign: 'left',
+  position: 'relative',
+  overflow: 'hidden',
+  paddingTop: sizes.regular,
+  paddingLeft: sizes.largeI,
+  paddingRight: sizes.regular,
+  paddingBottom: sizes.regular,
+  display: 'block',
+  textDecoration: 'none'
+}
+
+const styleSheet = StyleSheet.create({
+  listHeading: {
+    marginTop: sizes.regular,
+    ':first-child': {
+      marginTop: 0
+    }
+  },
+  card,
+  spacer: Object.assign({}, card, {
+    minHeight: '4.5rem',
+    lineHeight: '4.5rem'
+  }),
+  chevron: {
+    verticalAlign: 'middle',
+    transform: 'rotate(90deg)',
+    width: sizes.largeIi,
+    height: sizes.largeIi,
+    color: colors.black
+  },
+  titleContainer: {
+    display: 'flex'
+  },
+  title: {
+    color: colors.primary,
+    flexBasis: '100%',
+    lineHeight: 1
+  },
+  subtitle: {
+    display: 'inline-block',
+    fontWeight: typography.fontWeight.bold,
+    color: colors.midRed,
+    marginRight: sizes.regular,
+    marginTop: sizes.smallIii
+  },
+  descriptionParagraph: {
+    marginTop: sizes.regular,
+    maxWidth: '32rem',
+    ':first-child': {
+      marginTop: 0
+    }
+  },
+  heading: {
+    display: 'flex',
+    textAlign: 'left',
+    whiteSpace: 'nowrap',
+    alignItems: 'center',
+    ':before': {
+      width: sizes.regular,
+      content: '""',
+      height: '1px',
+      flexGrow: 1,
+      display: 'block',
+      backgroundColor: colors.greyLight,
+      marginRight: sizes.smallIi
+    },
+    ':after': {
+      content: '""',
+      height: '1px',
+      width: '100%',
+      flexGrow: 1,
+      display: 'block',
+      backgroundColor: colors.greyLight,
+      marginLeft: sizes.smallIi
+    }
+  }
+})
+
+module.exports = styleSheet
