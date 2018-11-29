4012ca0a7daf80410b802babc6c8e207646800d2
Timothy Robinson <timmyrobby@gmail.com>
2018-11-29 14:09:09 +0000

    minor: Add `fetchFormValues` helper function

---
+++ src/app/lib/fetch-form-values.js
@@ -0,0 +1,20 @@
+const { getFirstNonNil } = require('@nudj/library')
+
+function fetchFormValues ({ savedState, localState, fields, defaultValue = '' }) {
+  // Guard against null values
+  const localFormValues = localState || {}
+  const stateFormValues = savedState || {}
+
+  // Return an object that represents the values following this logic:
+  // If the value exists in the local changes, then that is the latest version.
+  // If that value doesn't exist in the local, but does exist in a stored version (i.e. fetched from db) use that.
+  // Otherwise, fall back on a default value (which defaults to an empty string)
+  return fields.reduce((fieldValues, field) => {
+    const value = getFirstNonNil(localFormValues[field], stateFormValues[field], defaultValue)
+    fieldValues[field] = value
+
+    return fieldValues
+  }, {})
+}
+
+module.exports = fetchFormValues
