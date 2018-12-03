2e8858941d3a087cf9635e8bc23178538cc096dd
Timothy Robinson <timmyrobby@gmail.com>
2018-12-03 13:05:49 +0000

    patch: Remove unused styles

--- src/app/pages/manage-survey-questions/style.css.js
+++ src/app/pages/manage-survey-questions/style.css.js
@@ -1,4 +1,4 @@
-const { StyleSheet, typography, sizes, colors, utilities } = require('@nudj/components/styles')
+const { StyleSheet, sizes, colors, utilities } = require('@nudj/components/styles')
 
 const styleSheet = StyleSheet.create({
   listItem: {
@@ -6,40 +6,7 @@ const styleSheet = StyleSheet.create({
     alignItems: 'stretch',
     listStyle: 'none',
     backgroundColor: colors.white,
-    boxShadow: utilities.boxShadow[10].narrow,
-    borderTopLeftRadius: '4px',
-    borderTopRightRadius: '4px',
-    ':nth-child(n+2)': {
-      borderTopColor: colors.greyLight,
-      borderTopWidth: '1px',
-      borderTopStyle: 'solid',
-      borderBottomLeftRadius: '0',
-      borderBottomRightRadius: '0',
-      borderTopLeftRadius: '0',
-      borderTopRightRadius: '0'
-    },
-    ':last-child': {
-      borderBottomLeftRadius: '4px',
-      borderBottomRightRadius: '4px'
-    },
-    ':first-child > :first-child ': {
-      borderTopLeftRadius: '4px',
-      borderBottomLeftRadius: '0'
-    },
-    ':nth-child(n+2) > :first-child': {
-      borderTopLeftRadius: '0',
-      borderBottomLeftRadius: '0'
-    },
-    ':last-child > :first-child': {
-      borderTopLeftRadius: 0,
-      borderBottomLeftRadius: '4px'
-    }
-  },
-  listHeading: {
-    marginTop: sizes.regular,
-    ':first-child': {
-      marginTop: 0
-    }
+    boxShadow: utilities.boxShadow[10].narrow
   },
   card: {
     textAlign: 'left',
@@ -71,42 +38,11 @@ const styleSheet = StyleSheet.create({
     flexBasis: '100%',
     lineHeight: 1
   },
-  subtitle: {
-    display: 'inline-block',
-    fontWeight: typography.fontWeight.bold,
-    color: colors.midRed,
-    marginRight: sizes.regular,
-    marginTop: sizes.smallIii
-  },
   descriptionParagraph: {
     marginTop: sizes.regular,
     maxWidth: '32rem',
     ':first-child': {
       marginTop: 0
-    }
-  },
-  heading: {
-    display: 'flex',
-    textAlign: 'left',
-    whiteSpace: 'nowrap',
-    alignItems: 'center',
-    ':before': {
-      width: sizes.regular,
-      content: '""',
-      height: '1px',
-      flexGrow: 1,
-      display: 'block',
-      backgroundColor: colors.greyLight,
-      marginRight: sizes.smallIi
-    },
-    ':after': {
-      content: '""',
-      height: '1px',
-      width: '100%',
-      flexGrow: 1,
-      display: 'block',
-      backgroundColor: colors.greyLight,
-      marginLeft: sizes.smallIi
     }
   }
 })
