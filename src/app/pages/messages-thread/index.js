c845b2f56323c0d5490997ed4bd85ef8bc4129ad
Nick Collings <nick@collingo.com>
2018-12-13 12:31:45 +0000

    patch: Rename emailAddress to email in accounts

--- src/app/pages/messages-thread/index.js
+++ src/app/pages/messages-thread/index.js
@@ -94,7 +94,7 @@ class MessageThreadPage extends React.Component {
                     firstName: messageFirstName,
                     lastName: messageLastName
                   } = getPersonOrConnectionName(message.from)
-                  const fromEmail = get(message, 'from.account.emailAddress', get(message, 'from.email'))
+                  const fromEmail = get(message, 'from.account.email', get(message, 'from.email'))
                   return (
                     <div className={css(style.threadSection)} key={message.id}>
                       <ThreadItem
